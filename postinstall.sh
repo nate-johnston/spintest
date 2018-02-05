070268e6f57481d4fc05cd556c729d121dc8a544
Nate Johnston <nate_johnston@cable.comcast.com>
2018-02-05 16:05:19 -0500

    Changes to work properly under Ubuntu

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 echo "The package was installed!  Yay!" > /etc/wowza
 cat /etc/wowza
 
-/bin/cp -v /app/nate/content.html /usr/share/nginx/html/index.html
-/sbin/service nginx restart
+/bin/cp -v /app/nate-test/content.html /usr/share/nginx/html/index.html
+/usr/sbin/service nginx restart

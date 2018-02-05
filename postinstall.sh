4021a8ef276519919642b441329aad7e67f90297
Nate Johnston <nate_johnston@cable.comcast.com>
2018-02-05 16:22:32 -0500

    trying to install nginx

--- postinstall.sh
+++ postinstall.sh
@@ -2,4 +2,5 @@ echo "The package was installed!  Yay!" > /etc/wowza
 cat /etc/wowza
 
 /bin/cp -v /app/nate-test/content.html /usr/share/nginx/html/index.html
+/usr/bin/apt-get -y install nginx
 /usr/sbin/service nginx restart

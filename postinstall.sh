74b91cbbe09074ddfe9e7cbb832886d6a198615a
Nate Johnston <nate_johnston@cable.comcast.com>
2018-02-05 16:38:32 -0500

    Fixing order of mkdir and cp

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 echo "The package was installed!  Yay!" > /etc/wowza
 cat /etc/wowza
 
-/bin/cp -v /app/nate-test/content.html /usr/share/nginx/html/index.html
 /bin/mkdir -p /usr/share/nginx/html 2>/dev/null
+/bin/cp -v /app/nate-test/content.html /usr/share/nginx/html/index.html
 /usr/sbin/service nginx restart

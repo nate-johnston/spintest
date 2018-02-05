5e7fb6fbbfe3300717b33438517c0e5b42b191c4
Nate Johnston <nate_johnston@cable.comcast.com>
2018-02-05 16:58:10 -0500

    Add nginx config file

--- postinstall.sh
+++ postinstall.sh
@@ -3,4 +3,5 @@ cat /etc/wowza
 
 /bin/mkdir -p /usr/share/nginx/html 2>/dev/null
 /bin/cp -v /app/nate-test/content.html /usr/share/nginx/html/index.html
+/bin/cp -v spintest.conf /etc/nginx/sites-enabled/spintest.conf
 /usr/sbin/service nginx restart

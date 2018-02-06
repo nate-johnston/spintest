7d1288cc8da836e0c2dfaab12e9c985a3adfeafc
Nate Johnston <nate_johnston@cable.comcast.com>
2018-02-06 11:07:35 -0500

    more fixes

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME=spintest
-VERSION=1.0.2
+VERSION=1.0.3
 REVISION=0
 PREFIX=/app/nate-test
 TARGET_DIR=.
--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,9 @@
 echo "The package was installed!  Yay!" > /etc/wowza
 cat /etc/wowza
 
-/bin/mkdir -p /usr/share/nginx/html 2>/dev/null
+/bin/mkdir -p /usr/share/nginx/html/healthcheck 2>/dev/null
 /bin/cp -v /app/nate-test/content.html /usr/share/nginx/html/index.html
+/bin/cp -v /app/nate-test/ok.html /usr/share/nginx/html/healthcheck/index.html
 /bin/cp -v /app/nate-test/spintest.conf /etc/nginx/sites-enabled/spintest.conf
+/bin/rm -f /etc/nginx/sites-available/default
 /usr/sbin/service nginx restart
